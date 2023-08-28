fdc06a0198c82c44d83ddc215b7d36b800178658
Analy Silva Dastek <analy.-@live.com>
2023-08-28 18:48:31 -0300

    trabajando en checkout y form

--- src/components/SendOrder.jsx
+++ src/components/SendOrder.jsx
@@ -1,18 +1,68 @@
-import React from 'react'
+import {collection, addDoc, getFirestore} from "firebase/firestore"
+import React, { useState } from 'react'
 
 const SendOrder = () => {
+
+//   const [name, setName] = useState ("")
+//   const [email, setEmail] = useState ("")
+//   const [orderId, setOrderId]= useState (null)
+
+  
+//   const handleSubmit =(e) => {
+//     e.preventDefault()
+//     addDoc(ordersCollection, order).then(({id})=>
+//     setOrderId (id))
+//   }
+  
+//   const order = {
+//     name,
+//     email
+//   }
+
+// const ordersCollection = collection (db, "orden")
+
+  const [orderId, setOrderId]= useState (null)
+
+const SendOrder = () => {
+  const buyer = {
+    buyer: {name: "Samuel", apellido: "Bile", email: "unmail@gmail.com"},
+    items: [{name: "producto A", price: 1000}],
+    total: 1000
+  }
+  
+  const db = getFirestore ()
+  const ordersCollection = collection (db, "orders")
+  
+  addDoc (ordersCollection, orderId).then (({id})=> setOrderId (id))
+}
+
+
+
+
+
+
+
+
   return (
-    <div>
+    <>
+      <h1> Enviando Ordenes  </h1>
 
-        <h1> Tu orden a sido enviada! </h1>
+      <form onSubmit={handleSubmit} >
+        <input type="text" placeholder="Nombre y Aellido" 
+        onChange={(e)=> setName(e.target.value)} />
 
-        <p>Aca va el formulario a completar por el usuario</p>
+        <input type="text" placeholder="Email" 
+        onChange={(e)=> setEmail(e.target.value)} />
 
-        <br/>
+        <button type="submit">  Enviar informacion </button>
+      </form>
+
+      <p> Tu codigo de orden es: {orderId}  </p>
+
+        
 
 
-
-    </div>
+    </>
   )
 }
 
